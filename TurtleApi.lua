5fc91b1a6b3f2c2459639a519da5931967d39b37
TomSmith27 <tom.smith2711@googlemail.com>
2016-07-30 01:05:12 +0100

    Add shaftMine and turtle api

---
+++ TurtleApi.lua
@@ -0,0 +1,13 @@
+function TryGetItem()
+    local success, data = turtle.inspect()
+    if success then
+        if data.name == "minecraft:stone"
+            or data.name == "minecraft:dirt"
+            or data.name == "minecraft:grass"
+            or data.name == "minecraft:gravel" then
+        else
+            print("Found : ", data.name)
+            turtle.dig()
+        end
+    end
+end
---
+++ shaftmine.lua
@@ -0,0 +1,30 @@
+local args = { ... }
+local lvls = tonumber(args[1])
+
+os.loadAPI("Minecraft-Scripts\TurtleApi")
+
+function MineLevel()
+    turtle.digDown()
+    turtle.down()
+    for i = 0, 3 do
+        turtle.turnLeft()
+        TryGetItem()
+    end
+end
+
+function MineToBottom()
+    for i = 1, lvls do
+        MineLevel()
+    end
+    for i = 1, lvls do
+        turtle.up()
+    end
+end
+if(turtle.getFuelLevel() < lvls * 2) then
+    print("Not enough fuel for mining")
+    print("Need ", lvls * 2)
+    print("Have " , turtle.getFuelLevel())
+else
+    MineToBottom()
+    turtle.placeDown(1)
+end
